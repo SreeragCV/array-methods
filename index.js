40d69c136087bffaf18ef35aa0d46614fc43639d
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-11-07 12:22:01 +0530

    added more methods

--- index.js
+++ index.js
@@ -17,8 +17,35 @@ function length() {
 //reverse(): This method reverses the order of the elements in the array.
 function reverse() {
   const array = ["apple", "orange", "grapes"];
-  const reverse = array.reverse();
-  console.log("reverse:", reverse);
+  array.reverse();
+  console.log("reverse:", array);
 }
 
-reverse();
+//sort(): This method sorts the elements of an array in place and returns the sorted array. It can take an optional compare function as an argument.
+function sort() {
+  const array = ["banana", "apple", "orange", "grapes"];
+  array.sort();
+  console.log("sort:", array);
+}
+
+//at(): This method returns the element at the specified index in the array. It takes one argument: the index.
+function at() {
+  const array = ["banana", "apple", "orange", "grapes"];
+  console.log("at:", array.at(1));
+}
+
+//fill(): This method fills all the elements of an array from a start index to an end index with a static value.
+//It can take up to three arguments: the value to fill with, the start index, and the end index.
+function fill() {
+  const array = ["banana", "apple", "orange", "grapes"];
+  array.fill("apple", 3, 0);
+  console.log(array);
+}
+
+//from(): This method creates a new array from an array-like object or an iterable object.
+//It can take up to two arguments: the object to convert to an array, and a mapping function to apply to each element of the new array.
+function from() {
+  const obj = { 0: "banana", 1: "apple", 2: "orange", 3: "grapes", length: 5 };
+  const array = Array.from(obj);
+  console.log(array);
+}
