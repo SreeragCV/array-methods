dc73e57b3e768e7903feb81e9bd0920bffcf7a30
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-11-13 16:14:01 +0530

    new methods added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//values(): This method returns an iterator that provides the values for each index in the array. It takes no arguments.
+// values(): This method returns an iterator that provides the values for each index in the array. It takes no arguments.
 function values() {
   const array = ["apple", "orange", "grapes"];
   const iterator = array.values();
@@ -8,65 +8,65 @@ function values() {
   }
 }
 
-//length(): This property returns the length of the array.
+// length(): This property returns the length of the array.
 function length() {
   const array = ["apple", "orange", "grapes"];
   console.log("length:", array.length);
 }
 
-//reverse(): This method reverses the order of the elements in the array.
+// reverse(): This method reverses the order of the elements in the array.
 function reverse() {
   const array = ["apple", "orange", "grapes"];
   array.reverse();
   console.log("reverse:", array);
 }
 
-//sort(): This method sorts the elements of an array in place and returns the sorted array.
-//It can take an optional compare function as an argument.
+// sort(): This method sorts the elements of an array in place and returns the sorted array.
+// It can take an optional compare function as an argument.
 function sort() {
   const array = ["banana", "apple", "orange", "grapes"];
   array.sort();
   console.log("sort:", array);
 }
 
-//at(): This method returns the element at the specified index in the array. It takes one argument: the index.
+// at(): This method returns the element at the specified index in the array. It takes one argument: the index.
 function at() {
   const array = ["banana", "apple", "orange", "grapes"];
   console.log("at:", array.at(1));
 }
 
-//fill(): This method fills all the elements of an array from a start index to an end index with a static value.
-//It can take up to three arguments: the value to fill with, the start index, and the end index.
+// fill(): This method fills all the elements of an array from a start index to an end index with a static value.
+// It can take up to three arguments: the value to fill with, the start index, and the end index.
 function fill() {
   const array = ["banana", "apple", "orange", "grapes"];
   array.fill("apple", 3, 0);
   console.log("fill:", array);
 }
 
-//from(): This method creates a new array from an array-like object or an iterable object.
-//It can take up to two arguments: the object to convert to an array, and a mapping function to apply to each element of the new array.
+// from(): This method creates a new array from an array-like object or an iterable object.
+// It can take up to two arguments: the object to convert to an array, and a mapping function to apply to each element of the new array.
 function from() {
   const obj = { 0: "banana", 1: "apple", 2: "orange", 3: "grapes", length: 5 };
   const array = Array.from(obj);
   console.log("from:", array);
 }
 
-//join(): This method joins all the elements of an array into a string using a specified separator.
-//It takes one optional argument: the separator to use.
+// join(): This method joins all the elements of an array into a string using a specified separator.
+// It takes one optional argument: the separator to use.
 function join() {
   const array = ["banana", "apple", "orange", "grapes"];
   const join = array.join("..! ");
   console.log("join:", join);
 }
 
-//toString(): This method returns a string representing the array and its elements.
+// toString(): This method returns a string representing the array and its elements.
 function toString() {
   const array = ["banana", "apple", "orange", "grapes"];
   const toString = array.toString();
   console.log("toString:", toString);
 }
 
-//pop(): This method removes the last element from an array and returns that element
+// pop(): This method removes the last element from an array and returns that element
 function pop() {
   const array = ["banana", "apple", "orange", "grapes"];
   const pop = array.pop();
@@ -74,9 +74,8 @@ function pop() {
   console.log("original array:", array);
 }
 
-//forEach() method executes a provided function once for each array element. It doesn't return anything,
-//it just executes the callback function on each element of the array.
-
+// forEach() method executes a provided function once for each array element. It doesn't return anything,
+// it just executes the callback function on each element of the array.
 function forEach() {
   const array = ["banana", "apple", "orange", "grapes"];
   array.map((element) => {
@@ -84,7 +83,7 @@ function forEach() {
   });
 }
 
-//shift() method removes the first element from an array and returns that removed element. This method changes the length of the array.
+// shift() method removes the first element from an array and returns that removed element. This method changes the length of the array.
 function shift() {
   const array = ["banana", "apple", "orange", "grapes"];
   const shift = array.shift();
@@ -92,74 +91,107 @@ function shift() {
   console.log("original array:", array);
 }
 
-//copyWithin() method shallow copies part of an array to another location in the same array and
-//returns the modified array without modifying its length. Syntax.copyWithin(target, start, end).
+// copyWithin() method shallow copies part of an array to another location in the same array and
+// returns the modified array without modifying its length. Syntax.copyWithin(target, start, end).
 function copyWithin() {
   let numbers = [1, 2, 3, 4, 5];
   numbers.copyWithin(2, 0, 2);
   console.log(numbers);
 }
 
-//push() method adds one or more elements to the end of an array and returns the new length of the array.
+// push() method adds one or more elements to the end of an array and returns the new length of the array.
 function push() {
-  const fruits = ['apple', 'orange', 'grapes', 'banana'];
-  const push = fruits.push('jackfruit');
+  const fruits = ["apple", "orange", "grapes", "banana"];
+  const push = fruits.push("jackfruit");
   console.log(push);
-  console.log('array:', fruits);
+  console.log("array:", fruits);
 }
 
-//unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+// unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
 function unshift() {
-  const fruits = ['apple', 'orange', 'grapes', 'banana'];
-  const unshift = fruits.unshift('jackfruit');
-  console.log('length:', unshift);
-  console.log('array:', fruits);
+  const fruits = ["apple", "orange", "grapes", "banana"];
+  const unshift = fruits.unshift("jackfruit");
+  console.log("length:", unshift);
+  console.log("array:", fruits);
 }
 
-//concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+// concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
 function concat() {
-  const fruits = ['apple', 'orange', 'grapes', 'banana'];
-  const vegetables = ['onion', 'carrot', 'cucumber'];
+  const fruits = ["apple", "orange", "grapes", "banana"];
+  const vegetables = ["onion", "carrot", "cucumber"];
   const concat = fruits.concat(vegetables);
-  console.log('new array:', concat);
+  console.log("new array:", concat);
 }
 
-//splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+// splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
 function splice() {
-  const fruits = ['apple', 'orange', 'grapes', 'banana'];
+  const fruits = ["apple", "orange", "grapes", "banana"];
   //const splice = fruits.splice(start, deleteCount, item1, item2, ...itemN);
   const splice = fruits.splice(1, 2);
-  console.log('splice:', splice);
-  console.log('array:', fruits);
+  console.log("splice:", splice);
+  console.log("array:", fruits);
 }
 
-//flat() This method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+// flat() This method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
 function flat() {
-  const fruits = ['apple',[ 'orange', ['grapes']], 'banana'];
+  const fruits = ["apple", ["orange", ["grapes"]], "banana"];
   const flat = fruits.flat();
-  console.log('flat-method:', flat);
-  console.log('array:', fruits);
+  console.log("flat-method:", flat);
+  console.log("array:", fruits);
 }
 
-//lastIndexOf() This method returns the last index at which a given element can be found in the array.
+// lastIndexOf() This method returns the last index at which a given element can be found in the array.
 function lastIndexOf() {
-  const numbers = [1,2,3,4,5,6,3];
+  const numbers = [1, 2, 3, 4, 5, 6, 3];
   const lastIndexOf = numbers.lastIndexOf(3);
-  console.log('lastIndexOf:', lastIndexOf);
-  console.log('array:', numbers);
+  console.log("lastIndexOf:", lastIndexOf);
+  console.log("array:", numbers);
 }
 
-//indexOf(): This method returns the index of the first occurrence of a specified element in an array. If the element is not present, 
-//it returns -1.
+// indexOf(): This method returns the index of the first occurrence of a specified element in an array. If the element is not present,
+// it returns -1.
 function indexOf() {
-  const numbers = [1,2,3,4,5,6,3];
+  const numbers = [1, 2, 3, 4, 5, 6, 3];
   const indexOf = numbers.indexOf(3);
-  console.log('indexOf:', indexOf);
+  console.log("indexOf:", indexOf);
 }
 
-//of(): This method creates a new array instance with a variable number of arguments, regardless of number or type of the arguments.
+// of(): This method creates a new array instance with a variable number of arguments, regardless of number or type of the arguments.
 function of() {
-  const arr = Array.of('string', 2, true, null, undefined);
+  const arr = Array.of("string", 2, true, null, undefined);
   console.log(arr);
 }
 
+// every(): This method checks if all elements in an array pass a test (provided as a function).
+// It returns true if all elements pass the test; otherwise, it returns false.
+function every() {
+  const arr = [2, 4, 6, 8, 10];
+  const isEven = (num) => num % 2 === 0;
+  const every = arr.every(isEven);
+  console.log(every);
+}
+
+// slice(): This method returns a shallow copy of a portion of an array into a new array object
+// selected from begin to end (end not included)
+function slice() {
+  const fruits = ["apple", "orange", "grapes", "banana", "mango", "kiwi", "melon", "watermelon"];
+  const slice = fruits.slice(1, 3);
+  console.log(slice);
+}
+
+// flatMap(): This method maps each element using a mapping function, then flattens the result into a new array.
+function slice() {
+  const numbers = [2, 4, 6, 8, 10];
+  const flatMap = numbers.flatMap(x => [x * 2]);
+  console.log(flatMap);
+
+}
+
+// findIndex(): This method returns the index of the first element in an array that passes a test (provided as a function). 
+// If no element passes the test, it returns -1.
+function findIndex() {
+  const numbers = [10, 20, 30, 40, 50, 60];
+  const greaterThan30 = (x) => x > 30;
+  const result = numbers.findIndex(greaterThan30);
+  console.log(result);
+}
